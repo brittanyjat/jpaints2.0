8f3d0db8a6e3a78dcae50a9bb33915ab4d77a92c
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-04-22 14:24:53 -0600

    re-doing jpaints!

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import About from './About';
+
+function App() {
+  return (
+    <div id="App">
+      <Header />
+      <About />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import lisa from '../assets/lisa.png';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div id='header'>
+                <div className='header-title'>
+                    <h1>JPaints Utah</h1>
+                    <div>
+                        <span>Thumbtack Top Pro</span>
+                        <span>{'Residential - Commerical - Exterior'}</span>
+                        <span>(385) 275-6993</span>
+                    </div>
+                </div>
+                <div className='header-nav'>
+                    <div>
+                        <span>About</span>
+                        <span>Services</span>
+                        <span>Portfolio</span>
+                        <span>Contact</span>
+                    </div>
+                </div>
+                <div className='hero' style={{ background: `linear-gradient(to right, rgba(125, 125, 125, 0.3), rgba(125, 125, 125, 0.3)), url(${lisa})` }}>
+                    {/* <span>Welcome</span> */}
+                </div>
+            </div>
+        )
+    }
+}
